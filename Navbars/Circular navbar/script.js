c0b913b839a6a5ec6ff6a27af6fd25c95d61ae28
suyogkokaje <suyogkokaje6@gmail.com>
2021-12-15 19:49:14 +0530

    feat: added circular navbar

---
+++ Navbars/Circular navbar/script.js
@@ -0,0 +1,5 @@
+let toggle = document.querySelector('.toggle');
+        let menu = document.querySelector('.menu');
+        toggle.onclick = function() {
+            menu.classList.toggle('active')
+        }
