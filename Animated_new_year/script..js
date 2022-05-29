fd1cd9a3eb43772c7c4dadf60758c3f8a3e74353
Amol shelke <shelkeamol897@gmail.com>
2022-05-29 12:19:24 +0530

    Added new Year countdown

--- Animated_new_year/script..js
+++
@@ -1,9 +0,0 @@
-const days = document.getElementById("days");
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
-const countdown = document.getElementById("countdown");
-const loading = document.getElementById("loading");
-const year = document.getElementById("year");
-
-const currentYear = new Date().getFullYear();
